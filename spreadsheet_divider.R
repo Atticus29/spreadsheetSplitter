c4a50c431976cd89eb48d3fc3bfae51b414f81ee
Mark <mark.aaron.fisher@gmail.com>
2017-11-17 12:10:33 -0800

    generalize the script a little more

--- spreadsheet_divider.R
+++ spreadsheet_divider.R
@@ -1,27 +1,41 @@
+#########################################
+##File name of spreadsheet to read from##
+#########################################
 fileString = "/Users/mf/2014_and_beyond/Academic/Teaching/Courses/PCC/Biology_101_Fall_2017/master_grades.csv" 
 
+################
+##Read in file##
+################
 require(data.table)
 data = read.csv(file =fileString , header=T, sep=",", stringsAsFactors = FALSE)
 data=as.data.table(data)
-cols = colnames(data)[1:16]
-dataAsOfPostLab6 = data[,cols,with=F]
 
-##################################################
-##The goal is to make this the only line to edit##
-##################################################
+#############
+##Variables##
+#############
+colSubset = colnames(data)[1:16] #TODO edit this as needed
+dataOnlyImportantCols = data[,colSubset,with=F]
+colsManualExclusion = colSubset[!colSubset %in% c("First_Name", "Last_Name","Raw_quiz2_out_of_9", "Quiz3_Grade_out_of_8")] #TODO edit this as needed
 
-colsNoRaw = cols[!cols %in% c("First_Name", "Last_Name","Raw_quiz2_out_of_9", "Quiz3_Grade_out_of_8")]
-
-
-dataShortened = data[,colsNoRaw, with =F]
+############################################
+##Apply rowSums and percentage calculation##
+############################################
+dataShortened = data[,colsManualExclusion, with =F]
 dataShortened[] <- lapply(dataShortened, function(x) as.numeric(as.character(x)))
+dataShortened[,Total := rowSums(.SD,na.rm=TRUE), .SDcols=colsManualExclusion]
+dataShortened[,TotalPercent := Total*100/120] #TODO edit the 120 for the total points as needed
+dataOnlyImportantCols = data.table(dataOnlyImportantCols, dataShortened[,c("Total", "TotalPercent"), with=F])
+lastRow = nrow(dataOnlyImportantCols)
 
-dataShortened[,Total := rowSums(.SD,na.rm=TRUE), .SDcols=colsNoRaw]
-dataShortened[,TotalPercent := Total*100/120]
+##############################################################
+##Perhaps there's a mean row to add to each sub spreadsheet?##
+##############################################################
+rowNumToIncludeInEach = lastRow #TODO edit this as needed
 
-dataAsOfPostLab6 = data.table(dataAsOfPostLab6, dataShortened[,c("Total", "TotalPercent"), with=F])
-lastRow = nrow(dataAsOfPostLab6)
-
+#########################################
+##Make the spreadsheet for each student##
+#########################################
 for(r in c(1:lastRow-1)){
-  write.table(as.data.frame(dataAsOfPostLab6[c(lastRow,r),]),file = paste("/Users/mf/2014_and_beyond/Academic/Teaching/Courses/PCC/Biology_101_Fall_2017/", dataAsOfPostLab6[r,Last_Name],"_grades.csv"), quote=FALSE, sep=",", col.names = TRUE, row.names = FALSE)
+  newFileName = paste("/Users/mf/2014_and_beyond/Academic/Teaching/Courses/PCC/Biology_101_Fall_2017/", dataOnlyImportantCols[r,Last_Name],"_grades.csv") #TODO edit this as needed
+  write.table(as.data.frame(dataOnlyImportantCols[c(rowNumToIncludeInEach,r),]),file = newFileName, quote=FALSE, sep=",", col.names = TRUE, row.names = FALSE)
 }
